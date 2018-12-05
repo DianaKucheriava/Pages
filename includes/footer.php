fb0e9994c6d654e7189f6848dbe1415008939d46
DianaKucheriava <Kucheriava.Diana@yandex.ru>
2018-12-05 15:38:27 +0200

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,13 @@
+        <script src="assets/js/jquery-1.11.1.js"></script>
+        <script src="assets/js/bootstrap.js"></script>
+        <script src="assets/js/vegas/jquery.vegas.min.js"></script>
+        <script src="assets/js/jquery.easing.min.js"></script>
+        <script src="assets/js/jquery.fancybox.js"></script>
+        <script src="assets/js/jquery.isotope.js"></script>
+        <script src="assets/js/appear.min.js"></script>
+        <script src="assets/js/animations.min.js"></script>
+        <script src="assets/js/custom.js"></script>
+        <script src="assets/js/jquery-migrate.min.js"></script>
+  </body>
+</html>
+<?php ob_end_flush(); ?><!--для блокування помилки Warning: Cannot modify header information -->
