4816a670da4688bcad2e3230d9f2ef04b6e82f0c
DianaKucheriava <Kucheriava.Diana@yandex.ru>
2018-12-05 15:37:44 +0200

    Delete comment.php

--- includes/comment.php
+++
@@ -1,6 +0,0 @@
-<?php
-       $page_id = 1;
-       $mysqli = new mysqli("localhost", "root", "", "Client");
-        $result_set = $mysqli->query("SELECT name, text_comment FROM `comments` WHERE `page_id`='$page_id'"); 
-          while ($row = $result_set->fetch_assoc()) {//print_r($row);
-			echo "<tr><td><b> " .$row['name']. "</b></td></tr> <tr><td> : <b>" .$row['text_comment']."</b></td></tr><br />";}  ?>
