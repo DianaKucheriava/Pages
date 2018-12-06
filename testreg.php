af8a6c9c91dbc222a7ce574a383251a338aec405
DianaKucheriava <Kucheriava.Diana@yandex.ru>
2018-12-06 13:10:45 +0200

    Update testreg.php

--- testreg.php
+++ testreg.php
@@ -1,13 +1,13 @@
 <?php
   include ("bd.php");
-    if (isset($_POST['submit'])) {
+    if (isset($_POST['submit'])) { //перевірка чи встановлені змінні
        $login = $_POST['login'];
        $password = $_POST['password'];
-       $query = mysqli_query($link,"SELECT * FROM users WHERE login='$login'");
-       $user_data= mysqli_fetch_array($query);
-              if ($user_data['password'] == $password) {
-                   header("Location:home.php"); exit();}
-                      else {
-                         echo "<form id='errorReg'><b>Помилковий логін\пароль!</b></form>";} 
+      $result=$link->query("SELECT * FROM users WHERE login='$login'");
+        $user_data=$result->fetch_array(MYSQLI_ASSOC);
+          if ($user_data['password'] == $password) {
+         header("Location:home.php"); exit();}
+    else {
+         echo "<form id='errorReg'><b>Помилковий логін\пароль!</b></form>";} 
     }
  ?>
