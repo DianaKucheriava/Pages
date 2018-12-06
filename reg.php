feef4138e4f86b5234af22ad74ed822051c43e34
DianaKucheriava <Kucheriava.Diana@yandex.ru>
2018-12-06 19:54:19 +0200

    Update reg.php

--- reg.php
+++ reg.php
@@ -6,20 +6,20 @@
                 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 animate-in" data-anim-type="fade-in-up">
                     <form method="POST" action="">
                     	<h3>Реєстрація</h3>
-							<p>
-							    <label>Ваш логін:*<br></label>
-							    <input name="login" type="text" size="15" maxlength="15">
-							</p>
-							<p>
-							    <label>Ваш пароль:<br></label>
-							    <input name="password" type="password" size="15" maxlength="15">
-							</p>
-							<p>
-							<input type="submit" name="submit" value="Зареєструватись" id="buttRegest">
-							</p>
-					</form>
-				</div>
-			</div>
+			<p>
+			    <label>Ваш логін:*<br></label>
+			    <input name="login" type="text" size="15" maxlength="15">
+			</p>
+			<p>
+			    <label>Ваш пароль:<br></label>
+			    <input name="password" type="password" size="15" maxlength="15">
+			</p>
+			<p>
+			    <input type="submit" name="submit" value="Зареєструватись" id="buttRegest">
+			</p>
+		   </form>
 		</div>
-	</section>
+	     </div>
+	 </div>
+    </section>
 <?php include("includes/footer.php"); ?>
