80dfbe7a16bbad20ce8548c534142d7ebcfc5f3e
DianaKucheriava <Kucheriava.Diana@yandex.ru>
2018-12-06 19:49:14 +0200

    Delete comment.php

--- includes/comment.php
+++
@@ -1,7 +0,0 @@
-<?php
-    if (isset($_REQUEST['ok'])) {
-     $string = '<tr><td><b>'.$_REQUEST['name'].'</b></td></tr> <tr><td> : <b>'.$_REQUEST['text_comment']. '</b></td></tr><br />';
-      file_put_contents('1.txt', $string, FILE_APPEND);
-      header("Location:".$_SERVER["HTTP_REFERER"]); }
-            include ('1.txt');
-?>
