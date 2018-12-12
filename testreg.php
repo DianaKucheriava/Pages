24b56d17e95b1e013957ef45ccfc23f57896b2e0
DianaKucheriava <Kucheriava.Diana@yandex.ru>
2018-12-12 13:50:16 +0200

    Update testreg.php

--- testreg.php
+++ testreg.php
@@ -4,8 +4,8 @@
        $err = [];
        $login = $_POST['login'];
        $password = $_POST['password'];
-       $result=$link->prepare("SELECT * FROM users WHERE login=?");
-       $result->bind_param('s', $login);
+       $result=$link->prepare("SELECT * FROM users WHERE login=? AND password=?");
+       $result->bind_param('ss', $login, $password);
        $result->execute();
        $user_data = $result->get_result()->fetch_array(MYSQLI_ASSOC);
      if ($user_data['password'] == $password){
