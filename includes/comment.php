10ea806e4dba3a416fb987b0118947e49f0566a5
DianaKucheriava <Kucheriava.Diana@yandex.ru>
2018-12-12 17:03:11 +0200

    Update comment.php

--- includes/comment.php
+++ includes/comment.php
@@ -1,7 +1,9 @@
 <?php
-	include('bd.php');
-	     $result = $link->query("SELECT * FROM comments");
-		 $all_comment=$result->fetch_array(MYSQLI_BOTH);
-		 do { echo "<b>".$all_comment['login']."</b> - <i>".$all_comment['text_comment']."</i><br />"; }
-		 while ($all_comment=$result->fetch_array(MYSQLI_BOTH));
+include('bd.php');
+ $result = $link->query("SELECT * FROM comments");
+ $all_comment=$result->fetch_array(MYSQLI_BOTH);
+ do { 
+	 echo "<b>".$all_comment['login']."</b> - <i>".$all_comment['text_comment']."</i><br />"; 
+    }
+ while ($all_comment=$result->fetch_array(MYSQLI_BOTH));
 ?>
