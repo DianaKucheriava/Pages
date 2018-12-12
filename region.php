7f7d1a45a94ef7063d0f8b3b0d8665688d910c13
DianaKucheriava <Kucheriava.Diana@yandex.ru>
2018-12-12 13:41:24 +0200

    Update region.php

--- region.php
+++ region.php
@@ -5,10 +5,11 @@
 		$result = array();
 		$res=$link->prepare("SELECT * FROM region WHERE id_country=?");
 		$res->bind_param('s', $id_country);
-				$res->execute();
-				$ress=$res->get_result();
-			while ($row = $ress->fetch_array(MYSQLI_BOTH)){
-			$result[]=array('id_region'=>$row['id_region'], 'region'=>$row['region']);
+		$res->execute();
+		$ress=$res->get_result();
+		while ($row = $ress->fetch_array(MYSQLI_BOTH))
+		{
+		  $result[]=array('id_region'=>$row['id_region'], 'region'=>$row['region']);
 		}
 		echo json_encode($result);
 	}
