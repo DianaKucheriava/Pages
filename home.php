77325f390e9cfb4c8f938cf88c2476d5b37ab494
DianaKucheriava <Kucheriava.Diana@yandex.ru>
2018-12-13 18:23:14 +0200

    Update home.php

--- home.php
+++ home.php
@@ -9,6 +9,7 @@
         <div class="col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2 ">
           <div class="services-wrapper">
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 animate-in" data-anim-type="fade-in-up">
+              <h3><a href="logout.php"> Вихід </a></h3>
               <h3>Ваші коментарі</h3>
               <div id="commentBlock">
                 <?php include("includes/comment.php"); ?>
@@ -16,7 +17,9 @@
               <form action="" method="post" name="form">
                 <table>
                   <tr>
-                    <td><label>Ім'я:</label><br /><input name="name" type="text" id="name"></td>
+                     <td><h3><?php echo $_SESSION['login']; ?></h3><br />
+                     <input name="name" type="hidden" id="name" disabled="true" value='<?php echo $_SESSION['id']; ?>'>
+                     </td>
                   </tr>
                   <tr>
                     <td><label>Коментар:</label><br /><textarea name="message" rows="5" cols="50" id="message"></textarea></td>
