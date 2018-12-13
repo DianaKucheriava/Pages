c801791224e2e97e62f2e0dcfab5170d0dbc6ea9
DianaKucheriava <Kucheriava.Diana@yandex.ru>
2018-12-13 18:24:21 +0200

    Add files via upload

---
+++ authorization.php
@@ -0,0 +1,22 @@
+<?php
+ session_start();
+  include ("bd.php");
+   if (isset($_POST['submit'])){  
+         $err = [];
+         $login = $_POST['login'];
+         $password = $_POST['password'];
+         $result=$link->prepare("SELECT * FROM users WHERE login=? AND password=?");
+         $result->bind_param('ss', $login, $password);
+         $result->execute();
+         $user_data = $result->get_result()->fetch_array(MYSQLI_ASSOC);
+     if ($user_data['password'] == $password){
+          $_SESSION['login']=$user_data['login'];
+          $_SESSION['id']=$user_data['id'];
+          header("Location:home.php"); 
+         exit();
+      }
+     else { 
+         $err[]="Помилковий логін\пароль !";
+      } 
+   } 
+  ?>
