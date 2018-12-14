6ced7ef4dc687eb8704e3be2b750dd34d4ad816a
DianaKucheriava <Kucheriava.Diana@yandex.ru>
2018-12-14 16:59:38 +0200

    Add files via upload

---
+++ assets/js/comment.js
@@ -0,0 +1,23 @@
+$(function() {
+  $("#send").click(function(){
+    var user_id = $("#nameuser").val();
+    var message = $("#message").val();  
+      $.ajax({
+        type: "POST",
+        url: "send_comment.php",
+        data: {"user_id": user_id, "message": message},
+        cache: false, 
+        dataType:"json",
+        success: function(data) {
+          if(data.success== true){
+            document.location.reload(true);
+          }else if(data.success == false){
+            $("#error").text(data.message).show().delay(1500).fadeOut(1000);
+          }else{
+            $("#error").text(data.message).show().delay(1500).fadeOut(800);
+          }
+        }
+      });
+    return false;
+  });
+});
