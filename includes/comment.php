e7a0f789227f14d98ebc36644a09994575a01230
DianaKucheriava <Kucheriava.Diana@yandex.ru>
2018-12-14 10:46:31 +0200

    Add files via upload

---
+++ includes/comment.php
@@ -0,0 +1,9 @@
+<?php
+	include('bd.php');
+	$result = $link->query("SELECT comments.*, users.login AS user FROM comments LEFT JOIN users ON users.user_id = comments.user_id");
+	$all_comment=$result->fetch_array(MYSQLI_BOTH);
+	do {
+		echo "<b>".$all_comment['user']."</b> - <i>".$all_comment['text_comment']."</i><br>".$all_comment['date']."<br />";
+	}
+	while ($all_comment=$result->fetch_array(MYSQLI_BOTH));
+?>
