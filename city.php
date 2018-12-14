ea90fa79c313a75a8794ac6c240eef0d093886a0
DianaKucheriava <Kucheriava.Diana@yandex.ru>
2018-12-14 12:03:27 +0200

    Add files via upload

---
+++ city.php
@@ -0,0 +1,15 @@
+<?php
+	include("bd.php");
+	if (isset($_POST['id_region']) && !empty($_POST['id_region'])){
+		$id_region = ($_POST['id_region']);
+		$result=array();
+		$res=$link->prepare("SELECT * FROM city WHERE id_region=?");
+		$res->bind_param('s', $id_region);
+		$res->execute();
+		$ress=$res->get_result();
+		while ($row=$ress->fetch_array(MYSQLI_BOTH)){
+			$result[]=array('id_city'=>$row['id_city'], 'city'=>$row['city']);
+		}
+		echo json_encode($result);			
+	}
+?>
