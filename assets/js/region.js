8e838aaa38e5a4c4a230c3c4482c0a758a6fbf24
DianaKucheriava <Kucheriava.Diana@yandex.ru>
2018-12-14 12:26:20 +0200

    Add files via upload

---
+++ assets/js/region.js
@@ -0,0 +1,27 @@
+$(function(){ 
+    var id_country=$(".country").val();
+    $.ajax({
+		type: "POST",
+		url:"region.php",
+		data: {id_country:id_country},
+		dataType: "json",
+	});
+	$(".country").change(function(){
+		var id_country=$(".country").val();
+		if (id_country==0) { }
+			$.ajax({
+				type: "POST",
+				url: "region.php",
+				data: {id_country:id_country},
+				dataType: "json",
+				success: function(data){
+					var selectHTML= "<select class='region' name='region'>";
+						for (var i = 0; i<data.length; i++) {
+							selectHTML	+= '<option value='+data[i].id_region+'>'+data[i].region+'</option>';
+						}
+						selectHTML+= "</select>";
+						$(".region").html(selectHTML);
+				}
+			});
+	});
+});
