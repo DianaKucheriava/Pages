ed6283863c1e68d57b0bb16c3a373d5a6244f92b
DianaKucheriava <Kucheriava.Diana@yandex.ru>
2018-12-14 16:58:56 +0200

    Add files via upload

---
+++ home.php
@@ -0,0 +1,45 @@
+<?php include("includes/header.php"); ?>
+<?php include("authorization.php"); ?>
+<div class="navbar-collapse collapse">
+  <ul class="nav navbar-nav navbar-right">
+    <li><a href="logout.php">Вихід</a></li>
+</ul>
+</div>
+</div></div>
+<section>
+  <div class="container">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="assets/js/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="assets/js/comment.js"></script>
+    <div class="row animate-in" data-anim-type="fade-in-up">
+      <div class="col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2 ">
+        <div class="services-wrapper">
+          <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 animate-in" data-anim-type="fade-in-up">
+            <form action="upload.php" method="post" enctype="multipart/form-data">
+              <input type=file name=uploadfile>
+              <input type=submit value=Загрузить>
+            </form>
+            <h3>Ваші коментарі </h3>
+            <div  id = "commentBlock">
+              <?php include("includes/comment.php"); ?>
+            </div>
+            <form action="" method="post" name="form" id="form">
+              <table>
+                <tr>
+                  <td><h3><?php echo $_SESSION['login']; ?></h3><br /><input name="name" type="hidden" id="name" disabled="true" value='<?php echo $_SESSION['user_id']; ?>'></td>
+                </tr>
+                <tr>
+                  <td><label>Залиште коментар:</label><span id="error"> </span><br /><textarea name="message" rows="5" cols="50" id="message"></textarea></td>
+                </tr>
+                <tr>
+                  <td><input name="js" type="hidden" value="no" id="js"><input name="button" type="submit" value="Коментувати" id="send"></td> 
+                </tr>
+              </table>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<?php include("includes/footer.php"); ?>
