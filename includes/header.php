7c716395861ac6d3a77510c5e857e69774354fa1
DianaKucheriava <Kucheriava.Diana@yandex.ru>
2018-12-14 12:47:32 +0200

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,13 @@
+<?php session_start();
+ob_start();   ?>
+<html>
+<head>
+<meta charset="utf-8" />
+<link href="assets/css/bootstrap.css" rel="stylesheet" />
+<link href="assets/css/animations.min.css" rel="stylesheet" />
+<link href="assets/css/style-green.css" rel="stylesheet" />
+<link href="assets/css/jquery.fancybox.css" rel="stylesheet" />
+</head>
+<body data-spy="scroll" data-target="#menu-section">
+	<div class="navbar navbar-inverse navbar-fixed-top scroll-me" id="menu-section" >
+		<div class="container">
