b47708fa0583b7900d4389c415f1c6151d9ffa54
DianaKucheriava <Kucheriava.Diana@yandex.ru>
2018-12-14 12:05:47 +0200

    Update country.php

--- country.php
+++ country.php
@@ -1,6 +1,6 @@
 <?php
 	$result=$link->query("SELECT * FROM country");
 	while ($row=$result->fetch_array(MYSQLI_BOTH)){
-	echo "<option value='{$row['id_country']}'>".$row['country']."</option>";
+		echo "<option value='{$row['id_country']}'>".$row['country']."</option>";
 	}
 ?>	
