5b3455797fbd8644e82487718290a7127a98f589
DianaKucheriava <Kucheriava.Diana@yandex.ru>
2018-12-17 20:00:59 +0200

    Update update_password.php

--- includes/update_password.php
+++ includes/update_password.php
@@ -3,18 +3,18 @@
 	$err = []; 
 	$user_id=$_SESSION['user_id'];
 	if (isset($_POST['update_password'])){  
-	    $old_password = $_POST['old_password'];
-	    $new_password = $_POST['new_password'];
+		$old_password = $_POST['old_password'];
+	    	$new_password = $_POST['new_password'];
 		$result=$link->prepare("SELECT * FROM users WHERE password=? AND user_id=?");
-	    $result->bind_param('ss', $old_password, $user_id);
-	    $result->execute();
-	    $user_data = $result->get_result()->fetch_array(MYSQLI_ASSOC);
-	    if ($user_data['password'] == $old_password){
-	        $update_passw=$link->query("UPDATE users SET password='$new_password' WHERE user_id='$user_id'");
-	       	$err[]="Пароль успішно змінений!" ;
-		}
-	    else { 
-	        $err[]="Ви ввели помилковий пароль !";
-	    } 
+	    	$result->bind_param('ss', $old_password, $user_id);
+	    	$result->execute();
+	    	$user_data = $result->get_result()->fetch_array(MYSQLI_ASSOC);
+	    		if ($user_data['password'] == $old_password){
+	        		$update_passw=$link->query("UPDATE users SET password='$new_password' WHERE user_id='$user_id'");
+	       			$err[]="Пароль успішно змінений!" ;
+			}
+	    		else { 
+	       	 		$err[]="Ви ввели помилковий пароль !";
+	    		} 
 	} 
 ?>
