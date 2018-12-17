198561ed0cac8e9945f711632ff7ce32c9af2b3e
DianaKucheriava <Kucheriava.Diana@yandex.ru>
2018-12-17 19:56:53 +0200

    Update comment.php

--- includes/comment.php
+++ includes/comment.php
@@ -1,7 +1,7 @@
 <?php
-include('bd.php');
-$result = $link->query("SELECT comments.*, img_src, users.login AS user FROM comments LEFT JOIN users ON users.user_id = comments.user_id ORDER BY num");
-$all_comment=$result->fetch_array(MYSQLI_BOTH);
+	include('bd.php');
+		$result = $link->query("SELECT comments.*, img_src, users.login AS user FROM comments LEFT JOIN users ON users.user_id = comments.user_id ORDER BY num");
+		$all_comment=$result->fetch_array(MYSQLI_BOTH);
 	do {
 		echo "<img id='smallAva' src=".$all_comment['img_src']."><b>".$all_comment['user']."</b> - <i>".$all_comment['text_comment']."</i><br>".$all_comment['date']."<br />";
 	}
